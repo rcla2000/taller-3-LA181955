3686d1d16647cf8c9cbc1d509ee691ffcf215713
Roberto Carlos López Abarca <rcla200031@gmail.com>
2022-08-23 22:09:38 -0600

    Arreglando descripción del portafolio

--- src/components/Descripcion.js
+++ src/components/Descripcion.js
@@ -8,8 +8,8 @@ class Descripcion extends React.Component {
         <p>
           Me gusta desarrollar software en especial el desarrollo web tanto la
           parte de backend como frontend. Mi lenguaje de programación favorito
-          es JavaScript. El framework de backend que más uso es Laravel de PHP.
-          Mi editor de código preferido es VS Code por tener una inmesidad de
+          es JavaScript y el framework de backend que más uso es Laravel de PHP.
+          Mi editor de código preferido es VS Code por tener una inmensidad de
           extensiones super útiles que facilitan y mejoran el desarrollo.
         </p>
       </div>
